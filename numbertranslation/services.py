fccc68db2419b8e3cbc615a0db219d4b0a13ad25
caioaraujo <c.felipe.araujo@gmail.com>
2019-04-17 21:17:16 -0300

    checkpoint

--- numbertranslation/services.py
+++ numbertranslation/services.py
@@ -4,9 +4,9 @@ from rest_framework.exceptions import NotAcceptable
 class NumberTranslationService:
 
     MAX_LIMIT_ERROR = 'Número acima do limite válido'
+    ZERO = 'zero'
 
     UNITS = {
-        '0': 'zero',
         '1': 'um',
         '2': 'dois',
         '3': 'três',
@@ -42,41 +42,106 @@ class NumberTranslationService:
         '9': 'noventa'
     }
 
-    def get_number_in_portuguese(self, number):
+    HUNDREDS = {
+        '2': 'duzentos',
+        '3': 'trezentos',
+        '4': 'quatrocentos',
+        '5': 'quinhentos',
+        '6': 'seiscentos',
+        '7': 'setecentos',
+        '8': 'oitocentos',
+        '9': 'novecentos'
+    }
+
+    THOUSANDS = {
+        '1': 'mil',
+        '2': 'dois mil',
+        '3': 'três mil',
+        '4': 'quatro mil',
+        '5': 'cinco mil',
+        '6': 'seis mil',
+        '7': 'sete mil',
+        '8': 'oito mil',
+        '9': 'nove mil'
+    }
+
+    def get_number_in_portuguese(self, number, result=""):
         """
         Given a number, returns it extension name in portuguese
 
         :param number: a valid number between -99999 and 99999
-        :return: string (number in portuguese)
+        :param result: the final result as string
+        :return: result
         """
-
-        result = ""
-
         number_as_str = str(number)
 
         # Check if the first char is a "-" sign
-        first_char = number_as_str[0]
-        if "-" == first_char:
-            result = "menos"
-
+        first_position = number_as_str[0]
+        if "-" == first_position:
             # Removes the negative sign from number
-            number_as_str = number_as_str[1::]
+            result = "menos"
+            return self.get_number_in_portuguese(number=number_as_str[1::], result=result)
 
         number_len = len(number_as_str)
 
-        if number_len == 1:
-            # Number is -9 to -1
-            result = ' '.join([result, self.UNITS[number_as_str]])
+        if number_len > 1 and self._is_zero_sequence(number_as_str):
             return result.strip()
 
-        if number_len == 2:
-            result = ' '.join([result, self._get_two_digits_number_in_extension(number_as_str)])
-            return result.strip()
-
-
+        if first_position == '0':
+            if number_len > 1:
+                return self.get_number_in_portuguese(number=number_as_str[1::], result=result)
+            if not result or result == '-':
+                return self.ZERO
 
         if number_len > 5:
             raise NotAcceptable(detail=self.MAX_LIMIT_ERROR)
+
+        if number_len == 5:
+            first_two_positions = number_as_str[0] + number_as_str[1]
+            result = ' '.join([result, self._get_two_digits_number_in_extension(first_two_positions), 'mil'])
+
+            return self.get_number_in_portuguese(number=number_as_str[2::], result=result)
+
+        if number_len == 4:
+            result = ' '.join([result, self.THOUSANDS[first_position]])
+
+            if self._is_zero_sequence(number_as_str[1::]):
+                return result.strip()
+            second_position = number_as_str[1]
+            third_position = number_as_str[2]
+            fourth_position = number_as_str[3]
+            if second_position == '0' or (third_position == '0' and fourth_position == '0'):
+                result = ' '.join([result, 'e'])
+
+            return self.get_number_in_portuguese(number=number_as_str[1::], result=result)
+
+        if number_len == 3:
+            # Number is in [-999, -100], [100, 999]
+            is_following_zeros = self._is_zero_sequence(number_as_str[1::])
+
+            if first_position == '1':
+                if is_following_zeros:
+                    result = ' '.join([result, 'cem'])
+                    return result.strip()
+                result = ' '.join([result, 'cento e'])
+                return self.get_number_in_portuguese(number=number_as_str[1::], result=result)
+            result = ' '.join([result, self.HUNDREDS[first_position]])
+            if is_following_zeros:
+                return result.strip()
+            result = ' '.join([result, 'e'])
+            return self.get_number_in_portuguese(number=number_as_str[1::], result=result)
+
+        if number_len == 2:
+            # Number is in [-99, -10], [10, 99]
+            result = ' '.join([result, self._get_two_digits_number_in_extension(number_as_str)])
+            return result.strip()
+
+        if number_len == 1:
+
+            # Number is in [-9, 9]
+            result = ' '.join([result, self.UNITS[number_as_str]])
+
+        return result.strip()
 
     def _get_two_digits_number_in_extension(self, numeric_string):
         first_position = numeric_string[0]
@@ -93,3 +158,10 @@ class NumberTranslationService:
             return result
         # Number has two digits, ending in a number 1-9
         return ' e '.join([result, self.UNITS[second_position]])
+
+    def _is_zero_sequence(self, numeric_string):
+        """ Returns true if numeric_string is composed by zeros """
+        for number in numeric_string:
+            if number != '0':
+                return False
+        return True
--- numbertranslation/tests.py
+++ numbertranslation/tests.py
@@ -5,11 +5,6 @@ from rest_framework.test import APITestCase
 class TestNumberTranslationAPI(APITestCase):
 
     def test_positive_numbers(self):
-        one_hundred_and_one = 101
-        three_hundred_and_five = 305
-        over_nine_thousand = 9814
-        twelve_thousand_five_hundred_and_fourty_two = 12542
-
         response = self.client.get(path='/0')
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual({'extenso': 'zero'}, response.data)
@@ -30,6 +25,46 @@ class TestNumberTranslationAPI(APITestCase):
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual({'extenso': 'quarenta e um'}, response.data)
 
+        response = self.client.get(path='/100')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual({'extenso': 'cem'}, response.data)
+
+        response = self.client.get(path='/119')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual({'extenso': 'cento e dezenove'}, response.data)
+
+        response = self.client.get(path='/608')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual({'extenso': 'seiscentos e oito'}, response.data)
+
+        response = self.client.get(path='/900')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual({'extenso': 'novecentos'}, response.data)
+
+        response = self.client.get(path='/1000')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual({'extenso': 'mil'}, response.data)
+
+        response = self.client.get(path='/1080')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual({'extenso': 'mil e oitenta'}, response.data)
+
+        response = self.client.get(path='/3400')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual({'extenso': 'três mil e quatrocentos'}, response.data)
+
+        response = self.client.get(path='/9999')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual({'extenso': 'nove mil novecentos e noventa e nove'}, response.data)
+
+        response = self.client.get(path='/10000')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual({'extenso': 'dez mil'}, response.data)
+
+        response = self.client.get(path='/20001')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual({'extenso': 'vinte mil e um'}, response.data)
+
     def test_negative_numbers(self):
         response = self.client.get(path='/-8')
         self.assertEqual(status.HTTP_200_OK, response.status_code)
@@ -43,13 +78,33 @@ class TestNumberTranslationAPI(APITestCase):
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual({'extenso': 'menos sessenta'}, response.data)
 
+        response = self.client.get(path='/-100')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual({'extenso': 'menos cem'}, response.data)
+
+        response = self.client.get(path='/-101')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual({'extenso': 'menos cento e um'}, response.data)
+
+        response = self.client.get(path='/-301')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual({'extenso': 'menos trezentos e um'}, response.data)
+
+        response = self.client.get(path='/-500')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual({'extenso': 'menos quinhentos'}, response.data)
+
+        response = self.client.get(path='/-232')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual({'extenso': 'menos duzentos e trinta e dois'}, response.data)
+
     def test_invalid_range(self):
         response = self.client.get(path='/111111')
         self.assertEqual(status.HTTP_406_NOT_ACCEPTABLE, response.status_code)
         self.assertEqual('Número acima do limite válido', response.data['detail'])
 
     def test_invalid_path(self):
-        response = self.client.get(path='/AAA')
+        response = self.client.get(path='/dois')
         self.assertEqual(status.HTTP_404_NOT_FOUND, response.status_code)
 
         response = self.client.get(path='/-')
